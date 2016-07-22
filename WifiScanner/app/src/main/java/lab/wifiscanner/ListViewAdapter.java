ea873bf3d4aee517b409aac171e94ae1cb6661be
Trần Xuân Nam <nam3vj@gmail.com>
2016-07-22 17:15:46 +0700

    version 1

---
+++ WifiScanner/app/src/main/java/lab/wifiscanner/ListViewAdapter.java
@@ -0,0 +1,61 @@
+package lab.wifiscanner;
+
+import android.content.Context;
+import android.net.wifi.ScanResult;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by namtr on 22/07/2016.
+ */
+public class ListViewAdapter extends BaseAdapter {
+    private LayoutInflater mInflater;
+    private ArrayList<ScanResult> data;
+
+    public ListViewAdapter(Context context, ArrayList<ScanResult> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.data = data;
+    }
+
+    @Override
+    public int getCount() {
+        return data.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return data.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        if (convertView == null) {
+            convertView = mInflater.inflate(R.layout.row, parent);
+            holder = new ViewHolder();
+            holder.SSID = (TextView) convertView.findViewById(R.id.ssid_tv);
+            holder.level = (TextView) convertView.findViewById(R.id.level_tv);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+        holder.SSID.setText(data.get(position).SSID);
+        holder.level.setText(data.get(position).capabilities);
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView SSID;
+        TextView level;
+    }
+}
--- WifiScanner/app/src/main/java/lab/wifiscanner/MainActivity.java
+++ WifiScanner/app/src/main/java/lab/wifiscanner/MainActivity.java
@@ -1,14 +1,75 @@
 package lab.wifiscanner;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    public WifiManager wifi;
+    public List<ScanResult> results;
+    public Button scanButton;
+    public ListView list;
+    public ListViewAdapter adapter;
+    public ArrayList<ScanResult> data = new ArrayList<>();
+    public BroadcastReceiver receiver;
+    public IntentFilter filter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // demo
+        init();
+        scanButton.setOnClickListener(this);
+        adapter = new ListViewAdapter(this, data);
+        list.setAdapter(adapter);
+        registerReceiver(receiver, filter);
+    }
+
+    public void init() {
+        wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        scanButton = (Button) findViewById(R.id.scan_button);
+        list = (ListView) findViewById(R.id.list_item);
+        receiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                results = wifi.getScanResults();
+            }
+        };
+        filter = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (!wifi.isWifiEnabled()) {
+            Toast.makeText(this, "Wifi is disabled...making it enabled", Toast.LENGTH_LONG).show();
+            wifi.setWifiEnabled(true);
+        }
+        Toast.makeText(this, "Scanning....", Toast.LENGTH_SHORT).show();
+        data.clear();
+        wifi.startScan();
+        results = wifi.getScanResults();
+        Toast.makeText(this, results.size() + " result", Toast.LENGTH_SHORT).show();
+        for (ScanResult result : results) {
+            data.add(result);
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(receiver);
     }
 }
