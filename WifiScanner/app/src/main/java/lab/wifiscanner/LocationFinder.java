c11ef099f9a5ae82fc9d45bb140ddeff095df5b8
Trần Xuân Nam <nam3vj@gmail.com>
2016-07-27 11:18:26 +0700

    update

--- WifiScanner/app/src/main/java/lab/wifiscanner/LocationFinder.java
+++ WifiScanner/app/src/main/java/lab/wifiscanner/LocationFinder.java
@@ -14,8 +14,6 @@ import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
 
 /**
