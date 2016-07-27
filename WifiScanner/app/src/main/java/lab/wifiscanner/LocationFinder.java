461938a74982e53b4d706ec4d59173711f7877e1
Trần Xuân Nam <nam3vj@gmail.com>
2016-07-27 11:17:17 +0700

    sửa lỗi ko mở cài đặt vị trí ở máy API < 23

--- WifiScanner/app/src/main/java/lab/wifiscanner/LocationFinder.java
+++ WifiScanner/app/src/main/java/lab/wifiscanner/LocationFinder.java
@@ -2,14 +2,18 @@ package lab.wifiscanner;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.widget.Toast;
@@ -19,19 +23,15 @@ import android.widget.Toast;
  */
 public class LocationFinder implements LocationListener {
     private Activity activity;
+    private LocationManager locationManager;
+    private String provider;
 
     public LocationFinder(Activity activity) {
         this.activity = activity;
-        LocationManager locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
+        locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
         Criteria criteria = new Criteria();
-        String provider = locationManager.getBestProvider(criteria, false);
-        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                || ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (!ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.ACCESS_COARSE_LOCATION)) {
-                ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION}, 0);
-                activity.startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
-            }
-        }
+        provider = locationManager.getBestProvider(criteria, false);
+        checkPermission();
         Location location = locationManager.getLastKnownLocation(provider);
         if (location != null) {
             onLocationChanged(location);
@@ -41,11 +41,36 @@ public class LocationFinder implements LocationListener {
         }
     }
 
+    public void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (activity.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                activity.requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+            } else if (activity.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                activity.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+            }
+        } else {
+            if (!locationManager.isProviderEnabled(provider)) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+                builder.setTitle("Location Service is not active");
+                builder.setMessage("Please enable your location services").setCancelable(false)
+                        .setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                activity.startActivity(intent);
+                            }
+                        });
+                builder.setPositiveButton("Cancel", null);
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        }
+    }
+
     @Override
     public void onLocationChanged(Location location) {
         double longitude = location.getLongitude();
         double latitude = location.getLatitude();
-        Toast.makeText(activity, String.valueOf(longitude + ":" + latitude), Toast.LENGTH_SHORT).show();
+        Toast.makeText(activity, String.valueOf(longitude + ":" + latitude), Toast.LENGTH_LONG).show();
     }
 
     @Override
