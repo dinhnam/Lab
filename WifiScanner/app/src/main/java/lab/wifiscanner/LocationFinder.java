db3d8cf6510c0476ca4cb2eec72a9b9911818eda
Trần Xuân Nam <nam3vj@gmail.com>
2016-07-28 13:43:02 +0700

    update

--- WifiScanner/app/src/main/java/lab/wifiscanner/LocationFinder.java
+++ WifiScanner/app/src/main/java/lab/wifiscanner/LocationFinder.java
@@ -1,85 +1,61 @@
 package lab.wifiscanner;
 
-import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.widget.Toast;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+
+import java.util.Locale;
 
 /**
  * Created by namtr on 27/07/2016.
  */
+@SuppressWarnings("MissingPermission")
 public class LocationFinder implements LocationListener {
-    private Activity activity;
-    private LocationManager locationManager;
-    private String provider;
-    double longitude;
-    double latitude;
+    public View view;
+    public LocationManager locationManager;
 
+    public LocationFinder(View view, LocationManager manager) {
+        this.view = view;
+        this.locationManager = manager;
+        checkGpsStatus();
+    }
 
-    public LocationFinder(Activity activity) {
-        this.activity = activity;
-        locationManager = (LocationManager) activity.getSystemService(Context.LOCATION_SERVICE);
-        Criteria criteria = new Criteria();
-        provider = locationManager.getBestProvider(criteria, false);
-        checkPermission();
-        Location location = locationManager.getLastKnownLocation(provider);
-        if (location != null) {
-            onLocationChanged(location);
+    public void checkGpsStatus() {
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Snackbar.make(view, "GPS is not available", Snackbar.LENGTH_LONG).setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent settingGPS = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    v.getContext().startActivity(settingGPS);
+                }
+            }).show();
         } else {
-            Toast.makeText(activity, "Location is null", Toast.LENGTH_SHORT).show();
-            locationManager.requestLocationUpdates(provider, 1000, 1, this);
+            getLocation();
         }
     }
 
-    public void checkPermission() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (activity.checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                activity.requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
-            } else if (activity.checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                activity.requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
-            }
+    public void getLocation() {
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location != null) {
+            onLocationChanged(location);
         } else {
-            if (!locationManager.isProviderEnabled(provider)) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-                builder.setTitle("Location Service is not active");
-                builder.setMessage("Please enable your location services").setCancelable(false)
-                        .setNegativeButton("OK", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialog, int id) {
-                                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                                activity.startActivity(intent);
-                            }
-                        });
-                builder.setPositiveButton("Cancel", null);
-                AlertDialog alert = builder.create();
-                alert.show();
-            }
+            Snackbar.make(view, "Location is null", Snackbar.LENGTH_LONG).show();
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 0.1f, this);
         }
     }
 
     @Override
     public void onLocationChanged(Location location) {
-        longitude = location.getLongitude();
-        latitude = location.getLatitude();
-        Toast.makeText(activity, String.valueOf(longitude + ":" + latitude), Toast.LENGTH_LONG).show();
-    }
-    public double getlongitude(){
-        return longitude;
-
-    }
-    public double getlatitude(){
-        return latitude;
-
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        String lng = String.format(Locale.getDefault(), "%.3f", longitude);
+        String ltd = String.format(Locale.getDefault(), "%.3f", latitude);
+        Snackbar.make(view, "Lng: " + lng + " <> Ltd: " + ltd, Snackbar.LENGTH_LONG).show();
     }
 
     @Override
--- WifiScanner/app/src/main/java/lab/wifiscanner/MainActivity.java
+++ WifiScanner/app/src/main/java/lab/wifiscanner/MainActivity.java
@@ -1,18 +1,21 @@
 package lab.wifiscanner;
 
 import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.LocationManager;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
-import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.design.widget.Snackbar;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -20,8 +23,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-@SuppressWarnings("MissingPermission")
-public class MainActivity extends Activity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, ActivityCompat.OnRequestPermissionsResultCallback {
     public WifiManager wifi;
     public List<ScanResult> results;
     public Button scanBtn;
@@ -29,34 +31,15 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public ListViewAdapter adapter;
     public ArrayList<ScanResult> data = new ArrayList<>();
     public boolean isScanning;
+    public LocationManager manager;
+    public View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        scanBtn.setOnClickListener(this);
-        adapter = new ListViewAdapter(this, data);
-        list.setAdapter(adapter);
-        final LocationFinder LF =new LocationFinder(this);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
-                showDialog(
-                        "SSID: " + data.get(position).SSID + "\n"
-                                + "BSSID: " + data.get(position).BSSID + "\n"
-                                + "Capabilities: " + data.get(position).capabilities + "\n"
-                                + "Frequency: " + data.get(position).frequency + "\n"
-                                + "Level: " + data.get(position).level + "\n"
-                        +"Longitude: " +LF.getlongitude()+"\n"
-                        +"Latitude: "+ LF.getlatitude()
-
-                );
-
-            }
-        });
-
+        checkPermission();
     }
 
     public void init() {
@@ -64,37 +47,75 @@ public class MainActivity extends Activity implements View.OnClickListener {
         wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
         scanBtn = (Button) findViewById(R.id.scan_button);
         list = (ListView) findViewById(R.id.list_item);
-        /* Ở phiên bản android có API >= 23
-         * Cần phải kiểm tra quyền truy cập vị trí (1 trong 2 quyền bên dưới) để lấy được danh sách Wifi*/
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
-        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+        scanBtn.setOnClickListener(this);
+        adapter = new ListViewAdapter(this, data);
+        list.setAdapter(adapter);
+        view = findViewById(R.id.main_layout);
+        manager = (LocationManager) getSystemService(LOCATION_SERVICE);
+    }
+
+    public void checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                Snackbar.make(view, "You need to allow access to GPS", Snackbar.LENGTH_LONG).setAction("Retry", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+                    }
+                }).show();
+                return;
+            }
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+            return;
+        }
+        new LocationFinder(view, manager);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 0 && grantResults.length >= 1) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                new LocationFinder(view, manager);
+            } else if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                    Snackbar.make(view, "You need to allow access to GPS again", Snackbar.LENGTH_LONG).setAction("Retry", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+                        }
+                    }).show();
+                } else {
+                    Snackbar.make(view, "Please making GPS enabled in the setting manually", Snackbar.LENGTH_LONG).setAction("OK", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent settingGPS = new Intent(Settings.ACTION_MANAGE_APPLICATIONS_SETTINGS);
+                            startActivity(settingGPS);
+                        }
+                    }).show();
+                }
+            }
         }
     }
 
     @Override
     public void onClick(View v) {
-        if(v.getId()==R.id.scan_button) {
-            if (isScanning) {
-                scanBtn.setText(String.valueOf("Scan Now"));
-                scanBtn.setTextColor(Color.BLACK);
-                isScanning = false;
-            } else {
-                scanBtn.setText(String.valueOf("Stop"));
-                scanBtn.setTextColor(Color.RED);
-                isScanning = true;
-            }
-            // Cần tạo 1 luồng riêng vì ta cần cập nhật giao diện khi quét được Wifi,
-            // nếu không ứng dụng sẽ bị treo
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    autoScan();
-                }
-            }).start();
+        if (isScanning) {
+            scanBtn.setText(String.valueOf("Scan Now"));
+            scanBtn.setTextColor(Color.BLACK);
+            isScanning = false;
+        } else {
+            scanBtn.setText(String.valueOf("Stop"));
+            scanBtn.setTextColor(Color.RED);
+            isScanning = true;
         }
-
+        // Cần tạo 1 luồng riêng vì ta cần cập nhật giao diện khi quét được Wifi,
+        // nếu không ứng dụng sẽ bị treo
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                autoScan();
+            }
+        }).start();
     }
 
     private void autoScan() {
@@ -131,18 +152,6 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 Toast.makeText(getBaseContext(), message, Toast.LENGTH_SHORT).show();
             }
         });
-    }
-    private void showDialog(String s){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Dialog");
-        builder.setMessage(s);
-        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
-        builder.show();
     }
 
 }
