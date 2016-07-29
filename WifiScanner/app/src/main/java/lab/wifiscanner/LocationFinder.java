b9ccfb9d0314317ea1119ab9c9496875a89c1c4e
Trần Xuân Nam <nam3vj@gmail.com>
2016-07-30 01:57:15 +0700

    lựa chọn vị trí theo gps và network

--- WifiScanner/app/src/main/java/lab/wifiscanner/LocationFinder.java
+++ WifiScanner/app/src/main/java/lab/wifiscanner/LocationFinder.java
@@ -1,5 +1,6 @@
 package lab.wifiscanner;
 
+import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
@@ -16,39 +17,94 @@ import java.util.Locale;
  */
 @SuppressWarnings("MissingPermission")
 public class LocationFinder implements LocationListener {
-    public View view;
-    public LocationManager locationManager;
-    public double longitude;
-    public double latitude;
+    private View view;
+    private LocationManager locationManager;
+    private double longitude;
+    private double latitude;
+    private static final long TWO_MINUTES = 120;
 
-    public LocationFinder(View view, LocationManager manager) {
+    public LocationFinder(View view, LocationManager locationManager) {
         this.view = view;
-        this.locationManager = manager;
-        checkGpsStatus();
+        this.locationManager = locationManager;
+        updateLocation();
     }
 
-    private void checkGpsStatus() {
-        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            Snackbar.make(view, "GPS is not available", Snackbar.LENGTH_LONG).setAction("OK", new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent settingGPS = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                    v.getContext().startActivity(settingGPS);
-                }
-            }).show();
-        } else {
-            getLocation();
-        }
-    }
-
-    private void getLocation() {
-        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    private void updateLocation() {
+        Location location = getLocation();
         if (location != null) {
             onLocationChanged(location);
         } else {
-            Snackbar.make(view, "Location is null", Snackbar.LENGTH_LONG).show();
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500, 0.1f, this);
+            Snackbar.make(view, "Location is null", Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                }
+            }).show();
         }
+    }
+
+    private Location getLocation() {
+        boolean isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        boolean isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        if (isGpsEnabled && isNetworkEnabled) {
+            Location gpsLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Location networkLocation = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+            return getBetterLocation(gpsLocation, networkLocation);
+        } else if (isGpsEnabled) {
+            return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        } else if (isNetworkEnabled) {
+            return locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+        Snackbar.make(view, "Location service is not available", Snackbar.LENGTH_LONG).setAction("OK", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent settingGPS = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                v.getContext().startActivity(settingGPS);
+            }
+        }).show();
+        return null;
+    }
+
+    private Location getBetterLocation(Location newLocation, Location currentBestLocation) {
+        if (newLocation == null && currentBestLocation == null) {
+            return null;
+        } else if (currentBestLocation == null) {
+            return newLocation;
+        } else if (newLocation == null) {
+            return currentBestLocation;
+        }
+        // Kiểm tra xem vị trí cố định mới là mới hơn hay cũ hơn
+        long timeDelta = newLocation.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+        boolean isNewer = timeDelta > 0;
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved.
+        if (isSignificantlyNewer) {
+            return newLocation;
+        } else if (isSignificantlyOlder) { // If the new location is more than two minutes older, it must be worse
+            return currentBestLocation;
+        }
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (newLocation.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider(newLocation.getProvider(), currentBestLocation.getProvider());
+        // Determine location quality using a combination of timeliness and accuracy
+        if (isMoreAccurate) {
+            return newLocation;
+        } else if (isNewer && !isLessAccurate) {
+            return newLocation;
+        } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+            return newLocation;
+        }
+        return currentBestLocation;
+    }
+
+    private boolean isSameProvider(String newProvider, String currentProvider) {
+        return newProvider.equals(currentProvider);
     }
 
     @Override
@@ -57,7 +113,11 @@ public class LocationFinder implements LocationListener {
         latitude = location.getLatitude();
         String lng = String.format(Locale.getDefault(), "%.3f", longitude);
         String ltd = String.format(Locale.getDefault(), "%.3f", latitude);
-        Snackbar.make(view, "Lng: " + lng + " <> Ltd: " + ltd, Snackbar.LENGTH_LONG).show();
+        Snackbar.make(view, "Lng: " + lng + " ~ Ltd: " + ltd, Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        }).show();
     }
 
     @Override
@@ -85,5 +145,7 @@ public class LocationFinder implements LocationListener {
 
     }
 
-
+    public void removeUpdate() {
+        locationManager.removeUpdates(this);
+    }
 }
